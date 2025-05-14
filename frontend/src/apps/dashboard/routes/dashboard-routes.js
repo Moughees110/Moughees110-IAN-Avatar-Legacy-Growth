cda16f42b051662542f4c18f506ceef749995b0b
waqaxhp <waqaxhp@gmail.com>
2025-05-15 03:10:58 +0500

    pushing so that raza can use my code

---
+++ frontend/src/apps/dashboard/routes/dashboard-routes.js
@@ -0,0 +1,18 @@
+// src/apps/dashboard/routes/dashboardRoutes.js
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import DashboardLayout from "../layouts/DashboardLayout";
+import DashboardHome from "../pages/DashboardHome";
+
+const DashboardRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<DashboardLayout />}>
+        <Route path="home" element={<DashboardHome />} />
+        {/* Add more dashboard-related pages here */}
+      </Route>
+    </Routes>
+  );
+};
+
+export default DashboardRoutes;
