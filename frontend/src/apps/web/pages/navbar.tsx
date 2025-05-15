ae05ab584afb5e97f14eaf68fabb5c0d5416749c
waqaxhp <waqaxhp@gmail.com>
2025-05-15 22:52:39 +0500

    Navbar login Sighnup Check in !
    Navbar login Sighnup Check in !

--- frontend/src/apps/web/pages/navbar.tsx
+++ frontend/src/apps/web/pages/navbar.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import { Menu, X } from 'lucide-react'
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import { Link } from "react-router-dom";
 
-
-const navItems = ['Features', 'Process', 'Pricing', 'Contact', 'Updates']
+const navItems = ["Features", "Process", "Pricing", "Contact", "Updates"];
 
 export default function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <header className="bg-[#0B0D11] text-white border-b border-[#1a1a1a] fixed top-0 w-full z-50">
@@ -30,12 +30,16 @@ export default function Navbar() {
 
         {/* CTA Button */}
         <div className="hidden md:flex">
-          <button className="flex items-center cursor-pointer gap-2 bg-[#121212] border border-[#1f1f1f] px-4 py-2 rounded-md hover:bg-[#1c1c1c] transition">
-            <span className="text-sm font-semibold">Login</span>
-          </button>
-          <button className="flex items-center cursor-pointer ml-2 gap-2 bg-[#121212] border border-[#1f1f1f] px-4 py-2 rounded-md hover:bg-[#1c1c1c] transition">
-            <span className="text-sm font-semibold">Sign Up</span>
-          </button>
+          <Link to="/auth/login">
+            <button className="flex items-center cursor-pointer gap-2 bg-[#121212] border border-[#1f1f1f] px-4 py-2 rounded-md hover:bg-[#1c1c1c] transition">
+              <span className="text-sm font-semibold">Login</span>
+            </button>
+          </Link>
+          <Link to="/auth/signup">
+            <button className="flex items-center cursor-pointer ml-2 gap-2 bg-[#121212] border border-[#1f1f1f] px-4 py-2 rounded-md hover:bg-[#1c1c1c] transition">
+              <span className="text-sm font-semibold">Sign Up</span>
+            </button>
+          </Link>
         </div>
 
         {/* Mobile Menu Button */}
@@ -68,5 +72,5 @@ export default function Navbar() {
         </div>
       )}
     </header>
-  )
+  );
 }
