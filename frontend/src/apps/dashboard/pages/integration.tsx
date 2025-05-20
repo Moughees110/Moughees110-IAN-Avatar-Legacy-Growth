caad0498cb9becacee0c536becb70d81b0905515
Akhtar Abbas <m.raza@huboweb.com>
2025-05-21 01:56:24 +0500

    check in

---
+++ frontend/src/apps/dashboard/pages/integration.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+function integration() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default integration
--- frontend/src/apps/dashboard/routes/dashboard-routes.tsx
+++ frontend/src/apps/dashboard/routes/dashboard-routes.tsx
@@ -4,19 +4,23 @@ import User from "../pages/user";
 import VoiceBotChat from "../pages/voicechat";
 import Dashhome from "../pages/dashhome";
 import LiveCall from "../pages/livecall";
+import Calendar from '../pages/calendar';
+import Integration from '../pages/integration';
 
 const DashboardRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<DashboardLayout />}>
         <Route index element={<Dashhome />} />
-        <Route path="voicebotchat" element={<VoiceBotChat />} />
+        <Route path="voice-bot-chat" element={<VoiceBotChat />} />
         <Route path="user" element={<User />} />
-        <Route path="livecall" element={<LiveCall />} />
+        <Route path="live-call" element={<LiveCall />} />
+        <Route path="calendar" element={<Calendar />} />
+        <Route path="integrations" element={<Integration />} />
         <Route path="*" element={<div>Page Not Found</div>} />
       </Route>
     </Routes>
-  );
+  )
 };
 
 export default DashboardRoutes;
